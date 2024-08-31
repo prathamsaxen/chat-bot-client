886e61b0be71ccadb1ac71c8fc178dd24b2bee1a
prathamsaxen <er.prathamsaxena@gmail.com>
2024-08-31 16:03:05 +0530

    First Commit with ChatBot!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Header from "./Components/Header";
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      type: "bot",
+      text: "Hi Jane, Amazing how Mosey is simplifying state compliance for businesses across the board!",
+    },
+  ]);
+
+  const [userInput, setUserInput] = useState("");
+
+  const handleUserInput = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (userInput.trim()) {
+      setMessages([
+        ...messages,
+        { type: "user", text: userInput },
+        {
+          type: "bot",
+          text: "Hi Jane, Amazing how Mosey is simplifying state compliance for businesses across the board!",
+        },
+      ]);
+      setUserInput("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="chatbot-container">
+        <Header/>
+        <div className="chatbot-body">
+          {messages.map((message, index) => (
+            <div key={index} className={`chat-message ${message.type === "user" ? "user" : "bot"}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="chatbot-footer">
+          <input
+            type="text"
+            value={userInput}
+            onChange={handleUserInput}
+            placeholder="Your question"
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RxCross2 } from "react-icons/rx";
+
+function Header() {
+  return (
+    <div className="chatbot-header">
+      {/* <h2>Hey👋, I'm Ava</h2>
+          <p>Ask me anything or pick a place to start</p> */}
+    </div>
+  );
+}
+
+export default Header;
