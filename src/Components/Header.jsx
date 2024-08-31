05c94d678ddfaebf6c8fa46635f71a9b1e952126
prathamsaxen <er.prathamsaxena@gmail.com>
2024-08-31 16:28:37 +0530

    Header Implemented!

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
+import { TbArrowsDiagonalMinimize2 } from "react-icons/tb";
+import img from "../assets/a.jpg";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="chatbot-header">
-      {/* <h2>Hey👋, I'm Ava</h2>
-          <p>Ask me anything or pick a place to start</p> */}
+      <div className="top-wrapper">
+        <TbArrowsDiagonalMinimize2 className="icons" />
+        <RxCross2 className="icons" />
+      </div>
+      <div className="profile">
+        <img src={img} alt="Error in loading..."/>
+        <h2>Hey👋,I'm Ava</h2>
+        <p>Ask me anything or pick a place to start</p>
+      </div>
     </div>
   );
 }
